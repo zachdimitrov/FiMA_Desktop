88546c8b54eb84963bc2af0362e164f753776d91
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2018-03-06 08:48:08 +0200

    finised test project

---
+++ FiMA.FrontOffice/EfCrudTest.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace FiMA.FrontOffice
+{
+    partial class EfCrudTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EfCrudTest));
+            this.lblCity = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblFamily = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtFamily = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.comboCities = new System.Windows.Forms.ComboBox();
+            this.EmployeeId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCity
+            // 
+            this.lblCity.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCity.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblCity.Location = new System.Drawing.Point(63, 85);
+            this.lblCity.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(34, 16);
+            this.lblCity.TabIndex = 3;
+            this.lblCity.Text = "City";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.lblFamily, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblCity, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblName, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblAddress, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtFamily, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtAddress, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnSave, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btnDelete, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btnCancel, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.dataGrid, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboCities, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(848, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblFamily
+            // 
+            this.lblFamily.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFamily.AutoSize = true;
+            this.lblFamily.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblFamily.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblFamily.Location = new System.Drawing.Point(43, 45);
+            this.lblFamily.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.lblFamily.Name = "lblFamily";
+            this.lblFamily.Size = new System.Drawing.Size(54, 16);
+            this.lblFamily.TabIndex = 2;
+            this.lblFamily.Text = "Family";
+            // 
+            // lblName
+            // 
+            this.lblName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblName.Location = new System.Drawing.Point(48, 5);
+            this.lblName.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(49, 16);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblAddress.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblAddress.Location = new System.Drawing.Point(31, 125);
+            this.lblAddress.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(66, 16);
+            this.lblAddress.TabIndex = 3;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtName
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtName, 2);
+            this.txtName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtName.Location = new System.Drawing.Point(103, 3);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(194, 22);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtFamily
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtFamily, 2);
+            this.txtFamily.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtFamily.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtFamily.Location = new System.Drawing.Point(103, 43);
+            this.txtFamily.Name = "txtFamily";
+            this.txtFamily.Size = new System.Drawing.Size(194, 22);
+            this.txtFamily.TabIndex = 7;
+            // 
+            // txtAddress
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtAddress, 2);
+            this.txtAddress.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtAddress.Location = new System.Drawing.Point(103, 123);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(194, 194);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(3, 323);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 35);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDelete.Location = new System.Drawing.Point(103, 323);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 35);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCancel.Location = new System.Drawing.Point(203, 323);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 35);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGrid.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.EmployeeId,
+            this.FirstName,
+            this.LastName});
+            this.dataGrid.GridColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGrid.Location = new System.Drawing.Point(303, 3);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.tableLayoutPanel1.SetRowSpan(this.dataGrid, 5);
+            this.dataGrid.Size = new System.Drawing.Size(542, 444);
+            this.dataGrid.TabIndex = 11;
+            this.dataGrid.DoubleClick += new System.EventHandler(this.dataGrid_DoubleClick);
+            // 
+            // comboCities
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.comboCities, 2);
+            this.comboCities.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboCities.FormattingEnabled = true;
+            this.comboCities.Location = new System.Drawing.Point(103, 83);
+            this.comboCities.Name = "comboCities";
+            this.comboCities.Size = new System.Drawing.Size(194, 24);
+            this.comboCities.TabIndex = 12;
+            this.comboCities.SelectedIndexChanged += new System.EventHandler(this.comboCities_SelectedIndexChanged);
+            // 
+            // EmployeeId
+            // 
+            this.EmployeeId.DataPropertyName = "Id";
+            this.EmployeeId.HeaderText = "Employee Id";
+            this.EmployeeId.Name = "EmployeeId";
+            this.EmployeeId.ReadOnly = true;
+            this.EmployeeId.Visible = false;
+            // 
+            // FirstName
+            // 
+            this.FirstName.DataPropertyName = "Name";
+            this.FirstName.HeaderText = "FirstName";
+            this.FirstName.Name = "FirstName";
+            this.FirstName.ReadOnly = true;
+            // 
+            // LastName
+            // 
+            this.LastName.DataPropertyName = "Family";
+            this.LastName.HeaderText = "Last Name";
+            this.LastName.Name = "LastName";
+            this.LastName.ReadOnly = true;
+            // 
+            // EfCrudTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(848, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "EfCrudTest";
+            this.Text = "Entity Framework CRUD Test";
+            this.Load += new System.EventHandler(this.EfCrudTest_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblFamily;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtFamily;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.ComboBox comboCities;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmployeeId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+    }
+}
+
---
+++ FiMA.FrontOffice/EfCrudTest.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FiMA.FrontOffice
+{
+    public partial class EfCrudTest : Form
+    {
+        public Employees model = new Employees();
+
+        public EfCrudTest()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Clear();
+        }
+
+        private void Clear()
+        {
+            this.txtAddress.Text = "";
+            this.txtName.Text = "";
+            this.txtFamily.Text = "";
+
+            this.btnSave.Text = "Save";
+            this.btnDelete.Enabled = false;
+
+            model.Id = 0;
+        }
+
+        private void EfCrudTest_Load(object sender, EventArgs e)
+        {
+            this.Clear();
+            using (EF_CRUD_TestEntities db = new EF_CRUD_TestEntities())
+            {
+                this.comboCities.DataSource = db.Cities.Select(x => x.Name).ToList();
+
+                // make it readonly
+                this.comboCities.DropDownStyle = ComboBoxStyle.DropDownList;
+
+                this.dataGrid.DataSource = db.Employees.Select(x => new { x.Id, x.Name, x.Family }).ToList();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            this.model.Name = txtName.Text;
+            this.model.Family = txtFamily.Text;
+            this.model.Address = txtAddress.Text;
+
+            using (EF_CRUD_TestEntities db = new EF_CRUD_TestEntities())
+            {
+                var city = db.Cities
+                    .Where(x => x.Name == (string)comboCities.SelectedValue)
+                    .FirstOrDefault();
+
+                int cityId = db.Cities
+                    .Where(x => x.Name == (string)comboCities.SelectedValue)
+                    .Select(x => x.Id)
+                    .FirstOrDefault();
+
+                model.CityId = cityId;
+
+                if (model.Id == 0)
+                {
+                    db.Employees.Add(model);
+                }
+                else
+                {
+                    db.Entry(model).State = EntityState.Modified;
+                }
+
+                db.SaveChanges();
+            }
+
+            populateDataGridView();
+            MessageBox.Show($"Employeee {this.txtName.Text} inserted to DB!");
+            this.Clear();
+        }
+
+        private void comboCities_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void populateDataGridView()
+        {
+            using (EF_CRUD_TestEntities db = new EF_CRUD_TestEntities())
+            {
+                this.dataGrid.DataSource = db.Employees.Select(x => new { x.Id, x.Name, x.Family }).ToList();
+            }
+        }
+
+        private void dataGrid_DoubleClick(object sender, EventArgs e)
+        {
+            if (dataGrid.CurrentRow.Index != -1)
+            {
+                model.Id = (int)dataGrid.CurrentRow.Cells["EmployeeId"].Value;
+                using (EF_CRUD_TestEntities db = new EF_CRUD_TestEntities())
+                {
+                    var empl = db.Employees.Where(x => x.Id == model.Id).FirstOrDefault();
+                    model = empl;
+                    txtAddress.Text = model.Address;
+                    txtFamily.Text = model.Family;
+                    comboCities.SelectedIndex = comboCities.FindString(db.Cities
+                        .Where(x => x.Id == model.CityId)
+                        .Select(y => y.Name)
+                        .FirstOrDefault());
+
+                    txtName.Text = model.Name;
+                }
+
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure?", "EF CRUD Operation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (EF_CRUD_TestEntities db = new EF_CRUD_TestEntities())
+                {
+                    var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                    {
+                        db.Employees.Attach(model);
+                    }
+
+                    db.Employees.Remove(model);
+                    db.SaveChanges();
+                }
+
+                populateDataGridView();
+                Clear();
+                MessageBox.Show("Employeee removed from DB!");
+            }
+        }
+    }
+}
